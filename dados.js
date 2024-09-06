0fbbc573f2f306c149daaf86f3bcc716174c2e55
kmgnanda <kmgnanda000@gmail.com>
2024-09-06 18:56:40 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,19 @@
+let dados = [
+    {
+    titulo: "Cara a Cara",
+     descricao: "Cara a Cara é um divertido jogo de tabuleiro que coloca a sua capacidade de dedução à prova! Com um tabuleiro repleto de rostos e perguntas estratégicas, o objetivo é descobrir qual personagem o seu adversário escolheu. Ao fazer perguntas e analisar as respostas, você eliminará as opções e se aproximará da solução.",
+    link: "https://mundotabuleiro.com/como-jogar-cara-a-cara/",
+    tags: "tabuleiro rosto personagem"
+  },
+  {
+    titulo: "Coup",
+    descricao: "Coup é um jogo de blefe e intriga para 2 a 6 jogadores. Cada jogador assume o papel de um nobre influente, com o objetivo de eliminar seus oponentes e ser o último jogador em pé. Com um punhado de cartas de personagens e ações, os jogadores podem blefar, assassinar, roubar e muito mais para alcançar a vitória.",
+    link: "https://mundotabuleiro.com/coup-jogo-de-tabuleiro/",
+    tags: "blefe cartas ações "
+  },
+  {
+    titulo: "Quix!",
+    descricao: "Quix! é um jogo de palavras rápido e divertido para até 8 jogadores. A cada rodada, os jogadores devem encontrar palavras que se encaixem em categorias e letras específicas, antes que o tempo acabe. Com mecânicas simples e um ritmo frenético, Quix! é perfeito para quem busca uma experiência de jogo leve e desafiadora.",
+    link: "https://mundotabuleiro.com/resenha-completa-de-quix-o-jogo-de-tabuleiro-que-vai-te-fazer-pensar-e-rir-ao-mesmo-tempo/",
+    tags: "tempo dasafiador palavras"
+  } ]
